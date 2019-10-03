7cc2b287b497d15beed86324ff66bbe981842b17
Andrew O'Grady <andrewogrady3@gmail.com>
2019-10-03 18:02:39 -0400

    getting started

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {Navbar} from './navbar'
 
 /**
  * COMPONENT
@@ -9,9 +10,12 @@ export const UserHome = props => {
   const {email} = props
 
   return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <div>
+        <h3>Welcome, {email}</h3>
+      </div>
+    </React.Fragment>
   )
 }
 
